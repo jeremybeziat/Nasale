091fb579488c32ced7b3038acb73913e00529cae
jeremybeziat <jeremybeziat.pro@gmail.com>
2022-05-05 20:37:22 +0200

    picture january and february

--- script.js
+++ script.js
@@ -29,10 +29,30 @@ function affichage(array, title) {
   return result;
 }
 
-APP.get("/Nasale/Picture", async (req, res) => {
+APP.get("/Nasale/January", async (req, res) => {
   try {
     const nasale_request = await fetch(
-      "https://api.nasa.gov/planetary/apod?api_key=gWIMl3cNb9eKccaobVNKWnzPwmOX18EiYfz3dXTN&start_date=2022-04-28"
+      "https://api.nasa.gov/planetary/apod?api_key=gWIMl3cNb9eKccaobVNKWnzPwmOX18EiYfz3dXTN&start_date=2022-01-01&end_date=2022-01-31"
+    );
+    const nasale_response = await nasale_request.json();
+    let infos = getNasaPicture(nasale_response);
+    let intox = getNasaTitle(nasale_response)
+    console.log(infos, intox);
+    let result = affichage(infos, intox);
+    res.status(200).send(
+    result
+    );
+  } catch (error) {
+    console.log("C'est la merde walla...");
+    res.status(500).send({ message: "❌ Erreur pour la requête de Nasale" });
+    console.log(error);
+  }
+});
+
+APP.get("/Nasale/February", async (req, res) => {
+  try {
+    const nasale_request = await fetch(
+      "https://api.nasa.gov/planetary/apod?api_key=gWIMl3cNb9eKccaobVNKWnzPwmOX18EiYfz3dXTN&start_date=2022-02-02&end_date=2022-02-28"
     );
     const nasale_response = await nasale_request.json();
     let infos = getNasaPicture(nasale_response);
